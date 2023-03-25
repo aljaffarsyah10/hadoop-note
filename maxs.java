9338da6b89dcdb30563325e88f6c41a3a0f14eb1
aljaffarsyah <aljaffarsyah10@gmail.com>
2023-03-25 23:27:14 +0700

    sabtu

--- maxs.java
+++ maxs.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 
 public final class maxs {
 
